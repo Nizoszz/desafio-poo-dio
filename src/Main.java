22347a7806ad8deb52196db2f9b79a5b270d7032
Andrew <andrewairamdasilva@gmail.com>
2023-09-01 12:06:57 -0300

    feat: added the logic of the functions in the classes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -8,17 +10,12 @@ public class Main {
         Curso curso = new Curso();
         curso.setTitulo("Java");
         curso.setDescricao("Curso de Java");
-        curso.setCargaHoraria("8hs");
+        curso.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("Python");
         curso2.setDescricao("Curso de Python");
-        curso2.setCargaHoraria("8hs");
-
-        Curso curso3 = new Curso();
-        curso3.setTitulo("JavaScript");
-        curso3.setDescricao("Curso de JavaScript");
-        curso3.setCargaHoraria("8hs");
+        curso2.setCargaHoraria(8);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Java");
@@ -30,11 +27,38 @@ public class Main {
         mentoria2.setDescricao("Mentoria de Python");
         mentoria2.setData(LocalDate.now());
 
-        Mentoria mentoria3 = new Mentoria();
-        mentoria3.setTitulo("JavaScript");
-        mentoria3.setDescricao("Mentoria de JavaScript");
-        mentoria3.setData(LocalDate.now());
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição do bootcamp Java developer");
+        bootcamp.setData(LocalDate.now());
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(mentoria);
 
-        System.out.println(curso);
+        Bootcamp bootcamp2 = new Bootcamp();
+        bootcamp2.setNome("Bootcamp Python Developer");
+        bootcamp2.setDescricao("Descrição do bootcamp Python developer");
+        bootcamp2.setData(LocalDate.now());
+        bootcamp2.getConteudos().add(curso2);
+        bootcamp2.getConteudos().add(mentoria2);
+
+        Dev devJoseph = new Dev();
+        devJoseph.setNome("Joseph");
+        devJoseph.inscreverBootcamp(bootcamp);
+        System.out.println("\nConteudos inscritos" + devJoseph.getConteudosInscritos());
+        devJoseph.progredir();
+        System.out.println("\nConteudos inscritos" + devJoseph.getConteudosInscritos());
+        System.out.println("\nConteudos concluidos" + devJoseph.getConteudosConcluidos());
+        System.out.println("XP: " + devJoseph.calcularXp());
+
+        Dev devMaria = new Dev();
+        devMaria.setNome("Maria");
+        devMaria.inscreverBootcamp(bootcamp2);
+        System.out.println("\nConteudos inscritos" + devMaria.getConteudosInscritos());
+        devMaria.progredir();
+        devMaria.progredir();
+        System.out.println("\nConteudos inscritos" + devMaria.getConteudosInscritos());
+        System.out.println("\nConteudos concluidos" + devMaria.getConteudosConcluidos());
+        System.out.println("XP: " + devMaria.calcularXp());
+
     }
 }
--- src/br/com/dio/desafio/dominio/Bootcamp.java
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -3,20 +3,14 @@ package br.com.dio.desafio.dominio;
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Bootcamp {
     private String nome;
     private String descricao;
     private final LocalDate dataInicio = LocalDate.now();
     private final LocalDate dataTermino = dataInicio.plusDays(45);
-    private Set<Dev> devsInscritos = new TreeSet<>();
+    private Set<Dev> devsInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudos = new LinkedHashSet<>();
-
-    public Bootcamp(String nome, String descricao) {
-        this.nome = nome;
-        this.descricao = descricao;
-    }
 
     public String getNome() {
         return nome;
@@ -42,14 +36,6 @@ public class Bootcamp {
         return conteudos;
     }
 
-    // public void adicionaConteudo(Conteudo conteudo) {
-    // this.conteudos.add(conteudo);
-    // }
-
-    // public void adicionaDev(Dev dev) {
-    // this.devsInscritos.add(dev);
-    // }
-
     @Override
     public boolean equals(Object o) {
         if (this == o)
@@ -63,6 +49,17 @@ public class Bootcamp {
     @Override
     public int hashCode() {
         return nome.hashCode();
+    }
+
+    public void setNome(String string) {
+        nome = string;
+    }
+
+    public void setDescricao(String string) {
+        descricao = string;
+    }
+
+    public void setData(LocalDate now) {
     }
 
 }
